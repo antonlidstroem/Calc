fb5e17c3b365ae9987782bcd6c9b978c99c749eb
Lidstroem <antonlidstroem@outlook.com>
2026-04-25 23:27:31 +0200

    Create NavigationService.cs

---
+++ Calc.Maui/Services/NavigationService.cs
@@ -0,0 +1,13 @@
+namespace Calc.App.Services;
+
+public class NavigationService : INavigationService
+{
+    public Task GoToAsync(string route, IDictionary<string, object>? parameters = null)
+    {
+        return parameters != null 
+            ? Shell.Current.GoToAsync(route, parameters) 
+            : Shell.Current.GoToAsync(route);
+    }
+
+    public Task GoBackAsync() => Shell.Current.GoToAsync("..");
+}
