0cd2256ead771ecfc82eb4e172d43ce8a5e2778f
Lidstroem <antonlidstroem@outlook.com>
2026-04-26 12:25:04 +0200

    Update App.xaml.cs

--- Calc.Maui/App.xaml.cs
+++ Calc.Maui/App.xaml.cs
@@ -1,3 +1,6 @@
+using Calc.App.Services;
+using Calc.App.Views;
+
 namespace Calc.App;
 
 public partial class App : Application
@@ -7,24 +10,28 @@ public partial class App : Application
     public App(SecurityService securityService)
     {
         InitializeComponent();
+
         _securityService = securityService;
+
+        RegisterRoutes();
+
         MainPage = new AppShell();
     }
 
-    protected override async void OnSleep()
-{
-    await _securityService.TriggerPanic();
-}
-
-
-    protected override void OnResume()
+    private void RegisterRoutes()
     {
-        // Säkerställ att vi är på CalculatorView när vi kommer tillbaka
-        _securityService.TriggerPanic().Wait();
+        Routing.RegisterRoute(nameof(ArticlesView), typeof(ArticlesView));
+        Routing.RegisterRoute(nameof(EditorView), typeof(EditorView));
+        Routing.RegisterRoute(nameof(ArticleDetailView), typeof(ArticleDetailView));
     }
 
-Routing.RegisterRoute(nameof(ArticlesView), typeof(ArticlesView));
-Routing.RegisterRoute(nameof(EditorView), typeof(EditorView));
-Routing.RegisterRoute(nameof(ArticleDetailView), typeof(ArticleDetailView));
+    protected override async void OnSleep()
+    {
+        await _securityService.TriggerPanic();
+    }
 
+    protected override async void OnResume()
+    {
+        await _securityService.TriggerPanic();
+    }
 }
