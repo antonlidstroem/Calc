d50aecba0c251538939d7a5631b34ff47432caad
Lidstroem <antonlidstroem@outlook.com>
2026-04-26 12:40:58 +0200

    Update AppShell.xaml.cs

--- Calc.Maui/AppShell.xaml.cs
+++ Calc.Maui/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace Calc.Maui
+namespace Calc.App;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
